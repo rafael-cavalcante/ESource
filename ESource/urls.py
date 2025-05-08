2b2fc6580b61c8b5fc48f40a2711a2bb4e5c08cd
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2025-05-07 23:55:33 -0300

    Atualizar e Deletar Evento

--- ESource/urls.py
+++ ESource/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('eventos', views.eventos, name='eventos'),
     path('artigo', views.artigo, name='artigo'),
+    path('cadastrarEvento', views.cadastrarEvento, name='cadastrarEvento'),
+    path('atualizarEvento/<int:eventoId>/', views.atualizarEvento, name='atualizarEvento'),
+    path('deletarEvento/<int:eventoId>/', views.deletarEvento, name='deletarEvento'),
 ]
--- ESource/views.py
+++ ESource/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from datetime import datetime
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Evento, Artigo
 
 # Create your views here.
@@ -25,3 +26,56 @@ def artigo(request):
     context = {'artigo' : artigo}
 
     return render(request, 'visualizarArtigo.html', context)
+
+def cadastrarEvento(request):
+    if request.method == "POST":
+        nome = request.POST.get('nome')
+        sigla = request.POST.get('sigla')
+        data = request.POST.get('data')
+        link = request.POST.get('link')
+
+        try:
+            data_evento = datetime.strptime(data, "%Y-%m-%d").date()
+        except ValueError:
+            data_evento = None  # ou lidar com erro
+
+        Evento.objects.create(
+            nome=nome,
+            sigla=sigla,
+            data=data_evento,
+            link=link
+        )
+
+        return redirect('ESource:eventos')  # ou outra view de sucesso
+
+    return render(request, 'cadastrarEvento.html')
+
+def atualizarEvento(request, eventoId):
+    evento = get_object_or_404(Evento, id=eventoId)
+
+    if request.method == "POST":
+        evento.nome = request.POST.get('nome')
+        evento.sigla = request.POST.get('sigla')
+        data = request.POST.get('data')
+        evento.link = request.POST.get('link')
+
+        try:
+            data_evento = datetime.strptime(data, "%Y-%m-%d").date()
+        except ValueError:
+            data_evento = None  # ou lidar com erro
+
+        evento.data = data_evento
+
+        evento.save()
+
+        return redirect('ESource:eventos')
+
+    context = {'evento': evento}
+
+    return render(request, 'cadastrarEvento.html', context)
+
+def deletarEvento(request, eventoId):
+    evento = get_object_or_404(Evento, id=eventoId)
+    evento.delete()
+
+    return redirect('ESource:eventos')
