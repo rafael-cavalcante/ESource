2437dfee200c4e54ed915d29129021b3f6a261d0
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2025-05-23 09:01:59 -0300

    Update settings.py

--- setup/settings.py
+++ setup/settings.py
@@ -35,7 +35,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+
+CSRF_TRUSTED_ORIGINS = [
+    os.getenv('CSRF_TRUSTED_ORIGINS'),
+]
 
 # Application definition
 
